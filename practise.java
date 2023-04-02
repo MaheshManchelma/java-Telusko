ba9c6c3d7b5f94b3e53c403230b48e30be529f65
Mahesh Manchelma <mk982309@gmail.com>
2023-04-02 13:50:21 +0100

    this is the practise code of github

---
+++ practise.java
@@ -0,0 +1,17 @@
+public class practise 
+{
+   public String name;
+        public int age;
+        public static void main(String[] args) {
+        
+    practise obj = new practise();
+    obj.name = "mahesh";
+    obj.age = 25;
+    System.out.println(obj.name + obj.age);
+      
+    }
+   
+
+
+
+}
